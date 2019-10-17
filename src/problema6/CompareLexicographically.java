98bfbd40a030aa51aa5eb462494c030014218a88
constantinb <baltag_costy@yahoo.com>
2019-10-17 13:25:45 +0100

    first commit

--- src/problema6/CompareLexicographically.java
+++ src/problema6/CompareLexicographically.java
@@ -14,65 +14,78 @@ public class CompareLexicographically {
 
 
         while (true) {
-            if(myList.size()==0){
-            try {
-                Scanner sc = getScannerFisiere("Introdu fisierul 1 - ");
+            if (myList.size() == 0) {
+                try {
 
-                while (sc.hasNextLine()) {
-                    myList.add(sc.nextLine());
+                    Scanner sc = getScannerFisiere("Introdu fisierul 1 - ");
+
+                    while (sc.hasNextLine()) {
+                        myList.add(sc.nextLine());
+                    }
+                    System.out.println("Continutul primului Fisier este ");
+
+                    for (int i = 0; i < myList.size(); i++) {
+
+                        System.out.print(" : " + myList.get(i));
+                    }
+                } catch (FileNotFoundException e) {
+                    System.out.println("Reintrodu alt path");
                 }
-                for (int i = 0; i < myList.size(); i++) {
-                    System.out.print(" : " + myList.get(i));
-                }
-            } catch (FileNotFoundException e) {
-                System.out.println("Reintrodu alt path");
-            }
             }
             if (myList.size() > 0) {
+
                 ArrayList<String> myList1 = new ArrayList<String>();
                 while (true) {
-                    try {
 
+
+                    try {
+                        System.out.println();
                         Scanner sc = getScannerFisiere("Introdu fisierul 2 - ");
                         while (sc.hasNextLine()) {
 
                             myList1.add(sc.nextLine());
                         }
+                        System.out.println("Continutul ultimului Fisier este ");
                         for (int i = 0; i < myList1.size(); i++) {
+
                             System.out.print(" : " + myList1.get(i));
                         }
 
                     } catch (FileNotFoundException e) {
                         System.out.println("Reintrodu alt path");
-
                     }
 
 
                     System.out.println();
-
                     if (myList.equals(myList1)) {
                         System.out.println("Listele egale");
+                        break;
+
                     } else {
-                        System.out.println("Ceva nu a mers bine");
+                        System.out.println("Ceva nu a mers bine si nu am putut compara listele te rugam mai incearca");
+                        break;
                     }
 
-
                 }
+                if (myList.equals(myList1)) {
+                    System.out.println("Bravo ai terminat");
+                    break;
+                }
+
+
             }
-
-                }
-
         }
-
-
-        private static Scanner getScannerFisiere (String s) throws FileNotFoundException {
-            System.out.println(s);
-            Scanner keyboard = new Scanner(System.in);
-            File file = new File(keyboard.nextLine());
-            return new Scanner(file);
-        }
-
     }
+
+
+    private static Scanner getScannerFisiere(String s) throws FileNotFoundException {
+        System.out.println(s);
+        Scanner keyboard = new Scanner(System.in);
+        File file = new File(keyboard.nextLine());
+        return new Scanner(file);
+    }
+
+}
 
 
 
